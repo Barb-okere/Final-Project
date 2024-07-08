c4a3c59e035b0e070ec405e2797c1bf1c152c335
Barb-okere <167083215+Barb-okere@users.noreply.github.com>
2024-07-08 01:28:18 -0700

    Create welcome.php

---
+++ welcome.php
@@ -0,0 +1,20 @@
+<?php
+// Initialize the session
+session_start();
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Welcome</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <div class="page-header">
+        <h1>Welcome to Estate Registration!</h1>
+    </div>
+    <p>
+        <a href="logout.php" class="btn btn-danger">Sign Out of Your Account</a>
+    </p>
+</body>
+</html>
